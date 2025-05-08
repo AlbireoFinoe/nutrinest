02f75f5b7bdebb881eeace00ed864b117e0f8ba6
AlbireoFinoe <albireo.m7@gmail.com>
2025-05-08 19:54:55 +0700

    login page

---
+++ lib/page/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class home_page_ extends StatefulWidget {
+  const home_page_({super.key});
+
+  @override
+  State<home_page_> createState() => _home_page_State();
+}
+
+class _home_page_State extends State<home_page_> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
